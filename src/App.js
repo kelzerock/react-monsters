8281eabb03f553d82cc826427112d5a746c1ed09
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-09-20 22:44:28 +0300

    feat: complete 29 lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from 'react';
+import { Component } from "react";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      name: 'Aleksei',
-    }
+      name: {
+        firstName: "Aleksei",
+        lastName: "Zhuchkov",
+      },
+      company: "ASB",
+    };
   }
 
-  render(){
+  render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}!</p>
-          <button onClick={()=>{
-            this.setState({name: 'Egor'})
-          }}>Change name</button>
+          <p>
+            Hello {this.state.name.firstName} {this.state.name.lastName}! You
+            are working in {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({
+                name: {
+                  firstName: "Egor",
+                  lastName: 'Zhuchkov'
+                },
+                company: 'xxx'
+              });
+              console.log(this.state);
+            }}
+          >
+            Change name
+          </button>
         </header>
       </div>
     );
