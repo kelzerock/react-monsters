3295c96824c7620664f02ac0b664189a6a192f93
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-09-22 00:35:32 +0300

    feat: complete 41 lesson

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchMonster: '',
     };
     console.log("constructor");
   }
@@ -18,25 +19,37 @@ class App extends Component {
         return data.json();
       })
       .then((user) =>
-        this.setState(() => {
-          return { monsters: user };
-        },
-        ()=>{console.log(this.state)})
+        this.setState(
+          () => {
+            return { monsters: user };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
       );
   }
 
   render() {
     console.log("render");
+    const newArrayMonsters = this.state.monsters.filter((el) => {
+      return el.name.toLowerCase().includes(this.state.searchMonster);
+    });
+    console.log(newArrayMonsters)
     return (
       <div className="App">
-      <input className="search-monster" type='search' plaseholder='search monster' onChange={(event)=>{
-        const eventValue = event.target.value.toLowerCase();
-        const newArrayMonsters = this.state.monsters.filter(el=> el.name.toLowerCase().includes(eventValue) )
-        this.setState(()=>{
-          return {monsters: newArrayMonsters}
-        })
-      }}/>
-        {this.state.monsters.map((element) => {
+        <input
+          className="search-monster"
+          type="search"
+          plaseholder="search monster"
+          onChange={(event) => {
+            const eventValue = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchMonster: eventValue };
+            });
+          }}
+        />
+        {newArrayMonsters.map((element) => {
           return (
             <div key={element.id}>
               <h1>{element.name}</h1>
