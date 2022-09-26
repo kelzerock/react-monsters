fa755a3f18b50df82c609ae60490bcd582dec092
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-09-27 00:25:37 +0300

    feat: complete 59 lesson

--- src/App.js
+++ src/App.js
@@ -1,55 +1,92 @@
 import "./App.css";
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchMonster: "",
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((data) => {
-        return data.json();
-      })
-      .then((user) =>
-        this.setState(() => {
-          return { monsters: user };
-        })
-      );
-  }
+      .then((data) => data.json())
+      .then((user) => setMonsters(user));
+  }, []);
 
-  onChangeSearch = (event) => {
-    const eventValue = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchMonster: eventValue };
+  useEffect(() => {
+    const newArrayMonsters = monsters.filter((el) => {
+      return el.name.toLowerCase().includes(searchField);
     });
+
+    setFilteredMonsters(newArrayMonsters);
+  }, [monsters, searchField]);
+
+  const onChangeSearch = (event) => {
+    const eventValue = event.target.value.toLowerCase();
+    setSearchField(eventValue);
   };
 
-  render() {
-    const { monsters, searchMonster } = this.state;
-    const { onChangeSearch } = this;
-    const newArrayMonsters = monsters.filter((el) => {
-      return el.name.toLowerCase().includes(searchMonster);
-    });
-    return (
-      <div className="App">
+  return (
+    <div className="App">
       <h1 className="app-title">Monster rolodex</h1>
-        <SearchBox
-          className="search-box-mosters"
-          placeholder="Search monster"
-          onChangeHandler={onChangeSearch}
-        />
-        <CardList monsters={newArrayMonsters} />
-      </div>
-    );
-  }
-}
+      <SearchBox
+        className="search-box-mosters"
+        placeholder="Search monster"
+        onChangeHandler={onChangeSearch}
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchMonster: "",
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch("https://jsonplaceholder.typicode.com/users")
+//       .then((data) => {
+//         return data.json();
+//       })
+//       .then((user) =>
+//         this.setState(() => {
+//           return { monsters: user };
+//         })
+//       );
+//   }
+
+//   onChangeSearch = (event) => {
+//     const eventValue = event.target.value.toLowerCase();
+//     this.setState(() => {
+//       return { searchMonster: eventValue };
+//     });
+//   };
+
+//   render() {
+//     const { monsters, searchMonster } = this.state;
+//     const { onChangeSearch } = this;
+//     const newArrayMonsters = monsters.filter((el) => {
+//       return el.name.toLowerCase().includes(searchMonster);
+//     });
+//     return (
+//       <div className="App">
+//       <h1 className="app-title">Monster rolodex</h1>
+//         <SearchBox
+//           className="search-box-mosters"
+//           placeholder="Search monster"
+//           onChangeHandler={onChangeSearch}
+//         />
+//         <CardList monsters={newArrayMonsters} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,7 +3,7 @@ import "./card.style.css";
 
 class Card extends Component {
   render() {
-    const { id, name, email } = this.props.monster;
+    const { name, email } = this.props.monster;
     return (
       <div className="card-container">
         <img
